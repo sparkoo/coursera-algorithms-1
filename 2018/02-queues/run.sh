8512704cf5646ffe74375984585ec07d6df97a59
Michal Vala <michal.vala@gmail.com>
2018-05-17 07:35:22 +0200

    passing Queues

--- 2018/02-queues/run.sh
+++ 2018/02-queues/run.sh
@@ -9,3 +9,7 @@ source ../setAndCompile.sh
 
 # run whatever here. example:
 # ${JAVA} -cp "${ALGS4JAR}${SEP}${OUT_DIR}" InteractivePercolationVisualizer 10
+
+# ${JAVA} -cp "${ALGS4JAR}${SEP}${OUT_DIR}" Deque
+# ${JAVA} -cp "${ALGS4JAR}${SEP}${OUT_DIR}" RandomizedQueue
+${JAVA} -cp "${ALGS4JAR}${SEP}${OUT_DIR}" Permutation 3 < ${DATA_DIR}/distinct.txt
