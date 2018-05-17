b8a22a2e62e0af533474dadd7dc0d0c4aa17d43a
Michal Vala <mvala@redhat.com>
2018-05-17 12:07:31 +0200

    find java sources with find command

--- 2018/setAndCompile.sh
+++ 2018/setAndCompile.sh
@@ -27,6 +27,6 @@ ALGS4JAR=${LIBS_DIR}/algs4.jar
 rm -rf ${OUT_DIR}
 mkdir -p ${OUT_DIR}
 
-javac -cp ${ALGS4JAR} -d out src/*.java ${DATA_DIR}/*.java
+javac -cp ${ALGS4JAR} -d out $( find . -name "*.java" )
 
 ls -l out
