d8bcfbae1da551467d16df624e3d316287aa6019
Michal Vala <michal.vala@gmail.com>
2018-06-08 07:04:27 +0200

    puzzle - implemented Board's hamming, manhattan, dimension, isGoal, equals, toString. TODO neighbors and twin

--- 2018/04-8puzzle/src/Board.java
+++ 2018/04-8puzzle/src/Board.java
@@ -1,30 +1,90 @@
+import edu.princeton.cs.algs4.In;
+
 public class Board {
+    private final int[][] blocks;
+
+    private final int moves;
+
+    private final int dimension;
+    private final int hamming;
+    private final int manhattan;
+
     /**
     construct a board from an n-by-n array of blocks
     (where blocks[i][j] = block in row i, column j)
     */
     public Board(int[][] blocks) {
+        this(blocks, 0, blocks.length);
+    }
 
-    } 
+    private Board(int[][] blocks, int moves, int dimension) {
+        this.moves = moves;
+        this.dimension = dimension;
+        this.blocks = blocksCopy(blocks);
+        this.hamming = calculateHamming(blocks);
+        this.manhattan = calculateManhattan(blocks);
+    }
+
+    private int[][] blocksCopy(int[][] blocks) {
+        int[][] copy = new int[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            System.arraycopy(blocks[i], 0, copy[i], 0, dimension);
+        }
+        return copy;
+    }
+
+    private int calculateHamming(int[][] blocks) {
+        int hamming = 0;
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks[i].length; j++) {
+                if (blocks[i][j] != 0 && blocks[i][j] != ((dimension * i) + j + 1)) {
+                    hamming++;
+                }
+            }
+        }
+        return hamming + moves;
+    }
+
+    private int calculateManhattan(int[][] blocks) {
+        int manhattan = 0;
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks[i].length; j++) {
+                if (blocks[i][j] == 0) {
+                    continue;
+                }
+                int calcI = (blocks[i][j] - 1) / dimension;
+                int calcJ = (blocks[i][j] - 1) % dimension;
+                if (i != calcI || j != calcJ) {
+                    int distanceI = i - calcI;
+                    manhattan += distanceI < 0 ? distanceI * -1 : distanceI;
+
+                    int distanceJ = j - calcJ;
+                    manhattan += distanceJ < 0 ? distanceJ * -1 : distanceJ;
+
+                }
+            }
+        }
+        return manhattan + moves;
+    }
 
     /** board dimension n */
     public int dimension() {
-        return 0;
+        return dimension;
     }
 
     /** number of blocks out of place */
     public int hamming() {
-        return 0;
+        return hamming;
     }
 
     /** sum of Manhattan distances between blocks and goal */
     public int manhattan() {
-        return 0;
+        return manhattan;
     }
 
     /** is this board the goal board? */
     public boolean isGoal() {
-        return false;
+        return hamming == 0;
     }
 
     /** a board that is obtained by exchanging any pair of blocks */
@@ -34,7 +94,29 @@ public class Board {
 
     /** does this board equal y? */
     public boolean equals(Object y) {
-        return false;
+        if (y == this) {
+            return true;
+        }
+        if (!(y instanceof Board)) {
+            return false;
+        }
+        Board that = (Board) y;
+
+        if (that.dimension != dimension ||
+            that.moves != moves ||
+            that.manhattan != manhattan ||
+            that.hamming != hamming) {
+            return false;
+        }
+
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks[i].length; j++) {
+                if (blocks[i][j] != that.blocks[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     /** all neighboring boards */
@@ -44,11 +126,37 @@ public class Board {
 
     /** string representation of this board (in the output format specified below) */
     public String toString() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks[i].length; j++) {
+                sb.append(blocks[i][j]);
+                sb.append(" ");
+            }
+            sb.append("\n");
+        };
+        return sb.toString();
     }
 
     /** unit tests (not graded) */
     public static void main(String[] args) {
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                blocks[i][j] = in.readInt();
+            }
+        }
+        Board initial = new Board(blocks);
+        blocks[n - 1][n - 1] = 3;
+        Board initial2 = new Board(blocks);
 
+        System.out.println(initial);
+        System.out.println("dimmension: " + initial.dimension);
+        System.out.println("hamming: " + initial.hamming);
+        System.out.println("manhattan: " + initial.manhattan);
+        System.out.println("goal: " + initial.isGoal());
+        System.out.println(initial.equals(initial2));
     }
 }
